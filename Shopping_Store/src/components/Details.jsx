c80619e9aa696b1af862e9715c0dba29d783c3cb
Parvaggarawal01 <parvaggarwal130@gmail.com>
2024-10-17 22:35:59 +0530

    Fetching single products and add in details component

--- Shopping_Store/src/components/Details.jsx
+++ Shopping_Store/src/components/Details.jsx
@@ -1,29 +1,52 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom";
+import axios from "../utils/axios";
+import Loading from "./Loading";
 
 function Details() {
-  return (
-    <div className="w-[70%] flex h-full items-center justify-center m-auto py-[10%] px-[5%]">
+  const [product, setProduct] = useState(null);
+  const { id } = useParams();
+  console.log(id);
+  const getsingleproducts = async () => {
+    try {
+        const {data} = await axios.get(`/products/${id}`)
+        setProduct(data);
+        
+    } catch (error) {
+        console.log(error);
+    }
+  }
+
+  useEffect( () => {
+    getsingleproducts();
+  }, [])
+  return product ? ( 
+    <div className="w-[70%] flex h-full items-center justify-between m-auto p-[10%]">
       <img
-        className="h-[70%] w-[50%] object-contain"
-        src="https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg"
+        className="h-[80%] w-[40%] object-contain"
+        src={`${product.image}`}
         alt=""
       />
       <div className="content w-[55%]">
         <h1 className="text-4xl">
-          Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops
+          {product.title}
         </h1>
-        <h3 className="text-zinc-500 my-5">men's clothing</h3>
-        <h2 className="text-red-300 mb-3">$ 109.95</h2>
+        <h3 className="text-zinc-500 my-5">{product.category}</h3>
+        <h2 className="text-red-300 mb-3">$ {product.price}</h2>
         <p className="mb-[5%]">
-          Your perfect pack for everyday use and walks in the forest. Stash your
-          laptop (up to 15 inches) in the padded sleeve, your everyday
+          {product.description}
         </p>
-        <Link className="mr-5 py-2 px-5 border rounded border-blue-200 text-blue-300">Edit</Link>
-        <Link className="py-2 px-5 border rounded border-red-200 text-red-300">Delete</Link>
+        <Link className="mr-5 py-2 px-5 border rounded border-blue-200 text-blue-300">
+          Edit
+        </Link>
+        <Link className="py-2 px-5 border rounded border-red-200 text-red-300">
+          Delete
+        </Link>
       </div>
     </div>
-  );
+  ) : (
+    <Loading />
+  )
 }
 
 export default Details;
--- Shopping_Store/src/components/Home.jsx
+++ Shopping_Store/src/components/Home.jsx
@@ -6,22 +6,21 @@ import Loading from "./Loading";
 
 function Home() {
   const [products] = useContext(ProductContext);
-  console.log(products);
+  // console.log(products);
 
   return products ? (
     <>
       <Nav />
       <div className="w-[85%]  p-10 pt-[5%] flex flex-wrap overflow-x-hidden overflow-y-auto">
         {products.map((p, i) => (
-          <Link
-            to="/details/1"
+          <Link key={p.id}
+            to={`/details/${p.id}`}
             className="mr-3 mb-3 card p-5 border shadow rounded w-[18%] h-[33vh] flex flex-col justify-center items-center"
           >
             <div
               className="hover:scale-110 w-full h-[75%] bg-contain bg-no-repeat bg-center mb-3"
               style={{
-                backgroundImage:
-                 `url(${p.image})`,
+                backgroundImage: `url(${p.image})`,
               }}
             ></div>
             <h1 className="hover:text-blue-300">{p.title}</h1>
