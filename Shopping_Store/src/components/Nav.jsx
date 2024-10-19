9f479ccd8de33ec70236178883d717f8d32698cc
Parvaggarawal01 <parvaggarwal130@gmail.com>
2024-10-19 21:43:50 +0530

    Adding products category in nav bar

--- Shopping_Store/src/components/Nav.jsx
+++ Shopping_Store/src/components/Nav.jsx
@@ -1,31 +1,47 @@
-import React from "react";
+import React, { useContext } from "react";
+import { ProductContext } from "../utils/Context";
+import { Link } from "react-router-dom";
 
 function Nav() {
+  const [products] = useContext(ProductContext);
+
+  let distinct_category =
+    products && products.reduce((acc, cv) => [...acc, cv.category], []);
+
+  distinct_category = [...new Set(distinct_category)];
+
+  const color = () => {
+    return `rgba(${(Math.random() * 255).toFixed()},${(
+      Math.random() * 255
+    ).toFixed()},${(Math.random() * 255).toFixed()},0.4)`;
+  };
+
+  // console.log(color());
+  
+
   return (
     <nav className="w-[15%] h-full bg-zinc-50 flex flex-col items-center pt-5">
-        <a
-          className="py-3 px-5 border rounded border-blue-200 text-blue-300"
-          href="/create"
-        >
-          Add New Products
-        </a>
-        <hr className="w-[80%] my-3" />
-        <h1 className="text-2xl w-[80%] mb-3">Category Filter</h1>
-        <ul className=" w-[80%]">
-          <li className="mb-3 flex items-center ">
-            <span className="rounded-full mr-2 w-[15px] h-[15px] bg-blue-300"></span>
-            Cat 1
-          </li>
-          <li className="mb-3 flex items-center ">
-            <span className="rounded-full mr-2 w-[15px] h-[15px] bg-red-300"></span>
-            Cat 1
-          </li>
-          <li className="mb-3 flex items-center ">
-            <span className="rounded-full mr-2 w-[15px] h-[15px] bg-green-300"></span>
-            Cat 1
-          </li>
-        </ul>
-      </nav>
+      <a
+        className="py-3 px-5 border rounded border-blue-200 text-blue-300"
+        href="/create"
+      >
+        Add New Products
+      </a>
+      <hr className="w-[80%] my-3" />
+      <h1 className="text-2xl w-[80%] mb-3">Category Filter</h1>
+      <div className=" w-[80%]">
+        {distinct_category.map((c, i) => (
+          <Link
+            key={i}
+            to={`/?category=${c}`}
+            className="mb-3 flex items-center "
+          >
+            <span style={{backgroundColor: color()}} className="rounded-full mr-2 w-[15px] h-[15px] "></span>{" "}
+            {c}
+          </Link>
+        ))}
+      </div>
+    </nav>
   );
 }
 
